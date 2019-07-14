fb0727f96d621c6e4ed73d47b9e2992f9bcae65b
Administrator <Administrator@SD-20181219OTXN>
2019-07-14 20:40:15 +0800

    666

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello github!!!22266666666666666666666666");
+		System.out.println("哈哈哈哈");
 	}
 
 }
